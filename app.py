e7bec8cde69b5fbb4227c2a93028126039501e97
KeerthanaPonnuru <50509527+KeerthanaPonnuru@users.noreply.github.com>
2023-10-27 11:50:19 -0400

    updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from detector import detect_faces
 from align import FaceAlign
 import threading
 from flask_cors import CORS
-
+import tkinter as tk
 
 app = Flask(__name__)
 CORS(app) 
@@ -16,11 +16,9 @@ session = onnxruntime.InferenceSession("swin_transformer.onnx")
 inname = [input.name for input in session.get_inputs()]
 outname = [output.name for output in session.get_outputs()]
 DYNAMIC_TEXT=''
-
+VAL=[]
 
 def frame_process(frame, input_shape=(224, 224)):
-    print('hi')
-
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, input_shape)
     image_mean = np.array([127, 127, 127])
@@ -32,29 +30,29 @@ def frame_process(frame, input_shape=(224, 224)):
 
 
 def get_prediction(image_data):
-
+    val=[]
     input = {
         inname[0]: image_data,
     }
     t0 = time.time()
     logits = session.run(outname, input)
-    #print(logits)
     logits= np.array(logits[0]).squeeze()
     logits = 1/(1 + np.exp(-logits))
     predict_time = time.time() - t0
-    #print("Predict Time: %ss" % (predict_time))
+    val=logits
+    val=[x * 100 for x in val]
     logits[logits>=0.5] = 1
     logits[logits<0.5] = 0
-    return logits
+    print("logits",logits)
+    return logits,val
 
 
 
-labels = np.array([1,2,4,5,6,7,9,10,12,14,15,17,20,23,24,25,26,27,43])
+labels = np.array([1,2,4,6,7,9,10,12,14,15,17,20,23,24,25,26,27,43])
 AU_DESCRIPTION = {
                     'AU1' : 'Inner Brow Raiser',
                     'AU2' : 'Outer Brow Raiser',
                     'AU4' : 'Brow Lowerer',
-                    'AU5' : 'Upper Lid Raiser',
                     'AU6' : 'Cheek Raiser',
                     'AU7' : 'Lid Tightener',
                     'AU9' : 'Nose Wrinkler',
@@ -73,30 +71,33 @@ AU_DESCRIPTION = {
                  }
 
 def write_on_frame(frame,outputs):
-
+    caption=[]  
+    #aus=np.array([])
+    #print("outputs",outputs)
     aus = labels[np.argwhere(outputs).squeeze()]
-    #print(aus)
+    aus=np.array(aus)
+
+    #if aus.ndim == 0:
+     #   aus = np.array([0], dtype=aus.dtype)
+    #print("aus",aus,type(aus))
+    #print(aus.dtype)
     window_name = 'Image'
   
-    # font
     font = cv2.FONT_HERSHEY_SIMPLEX
     
-    # fontScale
     fontScale = 1
     
-    # Blue color in BGR
     color = (0, 0, 255)
     
-    # Line thickness of 2 px
     thickness = 2
 
     for i, au in enumerate(aus):
+        print(i,"ok")
         org = (400, 100+(i+1)*50)
         au = AU_DESCRIPTION['AU'+str(au)]
-        #print(au)
+        caption.append(au)
         frame = cv2.putText(frame, au, org, font, fontScale, color, thickness, cv2.LINE_AA)
-        #print(frame)
-    return frame,au
+    return frame,caption
 
 
 
@@ -104,10 +105,11 @@ def fun():
     cap = cv2.VideoCapture(-0)
     obj=FaceAlign(244,2.9)
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  
-    videos = cv2.VideoWriter('ouput.mp4', fourcc, 2, (640, 480))
+    videos = cv2.VideoWriter('ouput.mp4', fourcc, 2, (1280, 960))
     min_face_size = 20.0
     thresholds=[0.6, 0.7, 0.8]
     nms_thresholds=[0.7, 0.7, 0.7]
+    x1,y1,x2,y2,z=0,0,0,0,0
     while (cap.isOpened()):
         ret, frame = cap.read()
         pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
@@ -125,33 +127,20 @@ def fun():
             ret, buffer = cv2.imencode('.jpg', frame)
             aligned_img=obj(pil_image, facial_landmarks)
             image = frame_process(frame)
-            outputs = get_prediction(image)
-
+            outputs,val = get_prediction(image)
             frame,dynamic_text = write_on_frame(frame,outputs)
-            print(dynamic_text,0)
-            dynamic_text_bytes = dynamic_text.encode('utf-8')
             if not ret:
                 continue
 
-            DYNAMIC_TEXT=dynamic_text
+            global VAL 
+            VAL=val
+            print(VAL)
+            global DYNAMIC_TEXT
+            DYNAMIC_TEXT = dynamic_text
             #print(DYNAMIC_TEXT,1)
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n' +
-                b'Content-Type: text/plain\r\n\r\n' + dynamic_text_bytes + b'\r\n')
-            #yield (jsonify({'caption': captions}),)
-            #yield (b'--frame\r\n'
-               #b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-            #yield (b'--frame\r\n'
-             #          b'Content-Type: text/plain\r\n\r\n' + dynamic_text.encode() + b'\r\n')
-            #return captions
-            #yield (b'--frame\r\n'
-             #  b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n'
-              # b'X-Text-Caption: ' + dynamic_text.encode() + b'\r\n')
-
-    #cap.release()
-
-#def text():
- #   return ''
+                b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+ 
 
 
 @app.route('/')
@@ -163,6 +152,25 @@ def index():
 def video():
     return Response(fun(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/captions')
+def cap():
+    global DYNAMIC_TEXT
+    response_data = {
+        'message': 'Data from the global variable',
+        'global_data': DYNAMIC_TEXT
+    }
+    return response_data
+
+@app.route('/percentage')
+def per():
+    global VAL
+    response_datas = {
+        
+        'global_data': VAL
+    }
+    return jsonify(response_datas)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
